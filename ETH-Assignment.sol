590cfd018a1bac758be19e62b4de4f3091a719f4
Demon <149153016+Shadowfight001@users.noreply.github.com>
2024-06-12 22:57:36 +0530

    Update ETH-Assignment.sol

--- ETH-Assignment.sol
+++ ETH-Assignment.sol
@@ -19,15 +19,18 @@ contract MyToken {
 
     // public variables here
 string public myTokenName="Udit";
-string public myTokenAbbr="Ud";
+string public myTokenAbbr="Udi";
 uint public totalSupply=0;
+
     // mapping variable here
 mapping (address => uint) public balance;
+
     // mint function
 function mint(address _address, uint _value) public{
     totalSupply= totalSupply + _value;
     balance[_address] += _value;
 }
+
     // burn function
 function burn(address _address, uint _value) public{
     if (balance[_address] >=_value){
